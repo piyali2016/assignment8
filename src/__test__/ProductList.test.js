b799868a16d0ebdebdab4c9e6cd30b550f6930d3
piyali2016 <piyali.acn@gmail.com>
2023-02-24 20:32:01 +0530

    Update ProductList.test.js

--- src/__test__/ProductList.test.js
+++ src/__test__/ProductList.test.js
@@ -1,59 +1,34 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import ProductList from './ProductList';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import courseReducer, { fetchCourseList } from './redux/courseSlice';
-
-const mockCourses = [
-  {
-    id: 1,
-    title: 'React Development',
-    img: 'https://example.com/react-dev.jpg',
-    Description: 'Learn to build web applications with React'
-  },
-  {
-    id: 2,
-    title: 'Node.js Development',
-    img: 'https://example.com/nodejs-dev.jpg',
-    Description: 'Learn to build server-side applications with Node.js'
-  }
-];
+import { store } from './redux/store';
+import ProductList from './ProductList';
 
 describe('ProductList', () => {
-  it('renders a list of courses', async () => {
-    const store = configureStore({
-      reducer: {
-        course: courseReducer
-      },
-      preloadedState: {
-        course: {
-          courses: mockCourses
-        }
-      }
-    });
-
+  it('should render the ProductList component', async () => {
     render(
       <Provider store={store}>
         <ProductList />
       </Provider>
     );
 
-    // Wait for courses to be fetched and rendered
     await waitFor(() => {
-      const courseCards = screen.getAllByRole('article');
-      expect(courseCards.length).toBe(2);
+      expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
+  });
 
-      const courseTitles = screen.getAllByRole('heading', { level: 5 });
-      expect(courseTitles[0]).toHaveTextContent('React Development');
-      expect(courseTitles[1]).toHaveTextContent('Node.js Development');
+  it('should render the course list', async () => {
+    render(
+      <Provider store={store}>
+        <ProductList />
+      </Provider>
+    );
 
-      const courseDescriptions = screen.getAllByRole('paragraph');
-      expect(courseDescriptions[0]).toHaveTextContent('Learn to build web applications with React');
-      expect(courseDescriptions[1]).toHaveTextContent('Learn to build server-side applications with Node.js');
-
-      const enquireButtons = screen.getAllByText('Enquire');
-      expect(enquireButtons.length).toBe(2);
+    await waitFor(() => {
+      expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+      expect(screen.getByText('Course 1')).toBeInTheDocument();
+      expect(screen.getByText('Course 2')).toBeInTheDocument();
+      expect(screen.getByText('Course 3')).toBeInTheDocument();
     });
   });
 });
