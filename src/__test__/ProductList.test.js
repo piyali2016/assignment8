1a711ca9bb21b16b2b93d935fa05d00b4b5bbd85
piyali2016 <piyali.acn@gmail.com>
2023-02-24 20:43:30 +0530

    Update ProductList.test.js

--- src/__test__/ProductList.test.js
+++ src/__test__/ProductList.test.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { configureStore } from '@reduxjs/toolkit';
 import ProductList from './ProductList';
+import { fetchCourseList } from './redux/courseSlice';
 
-describe('ProductList', () => {
-  it('should render the ProductList component', async () => {
+describe('ProductList component', () => {
+  test('renders a list of courses', async () => {
+    const courses = [
+      { id: 1, title: 'Course 1', Description: 'Description 1', img: 'image1.jpg' },
+      { id: 2, title: 'Course 2', Description: 'Description 2', img: 'image2.jpg' },
+      { id: 3, title: 'Course 3', Description: 'Description 3', img: 'image3.jpg' },
+    ];
+
+    const store = configureStore({
+      reducer: {
+        course: (state = { courses }, action) => {
+          switch (action.type) {
+            case fetchCourseList.type:
+              return { courses: action.payload };
+            default:
+              return state;
+          }
+        },
+      },
+    });
+
     render(
       <Provider store={store}>
         <ProductList />
       </Provider>
     );
 
-    await waitFor(() => {
-      expect(screen.getByText('Loading...')).toBeInTheDocument();
-    });
-  });
+    // Wait for the courses to load
+    await screen.findByText('Course 1');
 
-  it('should render the course list', async () => {
-    render(
-      <Provider store={store}>
-        <ProductList />
-      </Provider>
-    );
-
-    await waitFor(() => {
-      expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
-      expect(screen.getByText('Course 1')).toBeInTheDocument();
-      expect(screen.getByText('Course 2')).toBeInTheDocument();
-      expect(screen.getByText('Course 3')).toBeInTheDocument();
-    });
+    // Check that all the courses are rendered
+    expect(screen.getByText('Course 1')).toBeInTheDocument();
+    expect(screen.getByText('Course 2')).toBeInTheDocument();
+    expect(screen.getByText('Course 3')).toBeInTheDocument();
   });
 });
